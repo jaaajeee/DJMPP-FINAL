2974b0dccb76e657fc4be4919ddc735135ebd48b
Naphattra Sutthitham <wichuda.kru@gmail.com>
2022-04-25 02:51:24 +0700

    Initial commit

---
+++ backend/routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const recordRouter = require('./records.js');
+
+const router = express.Router();
+
+router.use('/me/record', recordRouter);
+
+router.post('/login', (req, res, next) => {});
+router.post('/signup', (req, res, next) => {});
+router.get('/me', (req, res, next) => {});
+router.put('/me', (req, res, next) => {});
+
+module.exports = router;
+
+
