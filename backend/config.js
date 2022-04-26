27f8aef20511befdbc8d9eafef33161618be2ef6
Jajee <wichuda.kru@gmail.com>
2022-04-27 01:52:01 +0700

    edit

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoUri: 'mongodb+srv://jajee:final1234@exercise-mern.gdkja.mongodb.net',
+    mongoOptions: {
+      user: 'jajee',
+      pass: 'final0123',
+      dbName: 'djmppdb',
+      retryWrites: true,
+      w: 'majority',
+    },
+  };
