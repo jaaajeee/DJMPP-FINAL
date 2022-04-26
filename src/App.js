e8b2a526f74a7004235c5313e52db0c65b4087e8
Naphattra Sutthitham <wichuda.kru@gmail.com>
2022-04-26 23:35:42 +0700

    add my newfrontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/navbar';
+import AddactivityPage from './pages/addactivityPage';
+import EditActivities from './components/exercise-form/edit-exercise';
+import ActivityListPage from './pages/activityListPage';
+import Profile from './components/profile';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbar />
+        <Profile />
+        <Routes>
+          <Route path="/create" exact element={<AddactivityPage />} />,
+          <Route path="edit/:id" element={<EditActivities />} />,
+          <Route path="/record" element={<ActivityListPage />} />
+        </Routes>
+      </Router>
   );
-}
-
-export default App;
+  }
+  export default App;
